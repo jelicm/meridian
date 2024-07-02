14d4db84cbf3bce4354c8ef3664b95f3f7d04688
Tamara Ranković <rankovictamaraa@gmail.com>
2024-07-02 11:05:15 +0200

    app model + app & ns store interfaces

--- internal/domain/namespace.go
+++ internal/domain/namespace.go
@@ -19,6 +19,22 @@ type ResourceQuota struct {
 	Quota        float64
 }
 
+type SeccompProfile struct {
+	Namespace    string
+	Application  string
+	Name         string
+	Version      string
+	Architecture string
+}
+
+func MakeNamespaceId(orgId, namespaceName string) string {
+	return fmt.Sprintf("%s/%s", orgId, namespaceName)
+}
+
+func MakeAppId(orgId, namespaceName, appName string) string {
+	return fmt.Sprintf("%s/%s", MakeNamespaceId(orgId, namespaceName), appName)
+}
+
 type Namespace struct {
 	orgId          string
 	name           string
@@ -53,7 +69,7 @@ func (n Namespace) GetProfileVersion() string {
 }
 
 func (n Namespace) GetId() string {
-	return fmt.Sprintf("%s/%s", n.orgId, n.name)
+	return MakeNamespaceId(n.orgId, n.name)
 }
 
 func (n Namespace) GetSeccompProfile() SeccompProfile {
@@ -66,10 +82,56 @@ func (n Namespace) GetSeccompProfile() SeccompProfile {
 	}
 }
 
-type SeccompProfile struct {
-	Namespace    string
-	Application  string
-	Name         string
-	Version      string
-	Architecture string
+type NamespaceTreeNode struct {
+	Namespace Namespace
+	Children  []Namespace
+}
+
+type NamespaceTree struct {
+	Root *NamespaceTreeNode
+}
+
+type App struct {
+	namespace      Namespace
+	name           string
+	resourceQuotas []ResourceQuota
+}
+
+func (a App) GetNamespace() Namespace {
+	return a.namespace
+}
+
+func (a App) GetName() string {
+	return a.name
+}
+
+func (a App) GetId() string {
+	return MakeAppId(a.namespace.orgId, a.namespace.name, a.name)
+}
+
+func (a App) GetResourceQuotas() []ResourceQuota {
+	quotas := make([]ResourceQuota, len(a.resourceQuotas))
+	copy(quotas, a.resourceQuotas)
+	return quotas
+}
+
+func (a *App) AddResourceQuota(quota ResourceQuota) error {
+	if !slices.Contains(supportedResourceQuotas, quota.ResourceName) {
+		return fmt.Errorf("quotas for a resource with name %s are not supported", quota.ResourceName)
+	}
+	a.resourceQuotas = append(a.resourceQuotas, quota)
+	return nil
+}
+
+type NamespaceStore interface {
+	Add(namespace, parent Namespace) error
+	Get(id string) (Namespace, error)
+	GetHierarchy(rootId string) (NamespaceTree, error)
+	Remove(id string) error
+}
+
+type AppStore interface {
+	Add(app App) error
+	FindByNamespace(namespaceId string) ([]App, error)
+	Remove(id string) error
 }
